c753c9106dca6e770b8a614ff9bb87d5e6a29107
Nico-Sellitto <nico.sellitto@mths.ca>
2024-09-17 11:11:40 -0400

    made the program calculate area and perimeter

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@
  *
  * Created by: Nico Sellitto
  * Created on: Sep 2024
- * This program says Hello, World!
+ * This program says calculates area and perimeter!
 */
 
-basic.showString('Hello, World!')
+basic.showString('a = 5 x 3 = ' + (5 * 3).toString() + ' cm^2')
+
+basic.showString('p = 2(5 + 3) = ' + (2 * (5 + 3)).toString() + ' cm')
